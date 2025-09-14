fa162f2a5bf84c30e6baf76e40ae1182a27a7f1f
AJMALAJU3 <ajmalaju3@users.noreply.github.com>
2025-09-14 15:47:18 +0530

    implemented using tesserat

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,14 +4,17 @@ import "./index.css";
 import ErrorBoundary from "./components/ErrorBoundary.tsx";
 import LandingPage from "./pages/LandingPage.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { AadhaarProvider } from "./context/AadharContext.tsx";
 
 const queryClient = new QueryClient();
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
+      <AadhaarProvider>
       <ErrorBoundary>
         <LandingPage />
       </ErrorBoundary>
+      </AadhaarProvider>
     </QueryClientProvider>
   </StrictMode>
 );
--- client/src/pages/LandingPage.tsx
+++ client/src/pages/LandingPage.tsx
@@ -4,6 +4,7 @@ import ImageUpload from "../components/ImageUpload";
 import AadhaarLogo from "../assets/images/Aadhaar_Logo.svg.png";
 
 const LandingPage = () => {
+
   return (
     <div className="landing-page">
       <div className="landing-header">
